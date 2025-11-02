7b911f96c18fe7f95ba11401e267b6a00181f345
Szzz666 <3214948198@qq.com>
2025-11-03 00:28:14 +0800

    feat(index): 添加娱乐频道链接
    - 在首页添加了"其他"按钮，链接至娱乐频道
    - 调整了按钮布局顺序，将"频道"按钮位置提前
    - 更新了JavaScript文件，为新按钮添加点击事件处理
    - 新增娱乐频道URL: http://mc.szzz666.top:23400/

--- js/clickevent.js
+++ js/clickevent.js
@@ -180,4 +180,7 @@ $(document).ready(function () {
     $(".xzapp").click(function () {
         window.open("https://github.com/szzz666/StarrySkyServerOfficialWebsite/releases/download/%E6%98%9F%E7%A9%BA%E6%9C%8Dapp/StarrySkyServer_v1.8.5.apk");
     });
+    $(".yule").click(function () {
+        window.open("http://mc.szzz666.top:23400/");
+    });
 });
