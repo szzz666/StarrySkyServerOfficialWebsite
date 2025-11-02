4b59a6330fd6073767d15bf3dfaf9de1ec4af296
Szzz666 <3214948198@qq.com>
2025-11-03 00:21:37 +0800

    feat(api): 更新在线状态获取接口地址- 将请求地址从 "https://motdbe.blackbe.work/api" 更改为 "https://motd.imc.re/api" - 更新目标主机参数为 "mc.szzz666.top:20000"

--- js/getonline3.js
+++ js/getonline3.js
@@ -9,7 +9,7 @@ new Vue({
     },
     methods: {
         getonline() {
-            axios.get("https://motd.imc.re/api?host=mc.szzz666.top:20000")
+            axios.get("https://motdbe.blackbe.work/api?host=mc.szzz666.top:20000")
                 .then(result => {
                     this.online = result.data.online;
                     setTimeout(this.getonline, 10000);
