0ab613ddcfcc222edcf6b412302304c04dc3337f
世中之中 <60167194+szzz666@users.noreply.github.com>
2026-03-12 22:18:25 +0800

    自动添加到服务器列表端口改为19132
    自动添加到服务器列表端口改为19132

--- js/clickevent.js
+++ js/clickevent.js
@@ -125,7 +125,7 @@ $(document).ready(function () {
     //加入游戏
     $(".jiaruyouxi").click(function () {
         tipsmessage("正在添加服务器到你的游戏服务器列表内");
-        window.open("minecraft://?addExternalServer=StarrySky|mc.szzz666.top:44966");
+        window.open("minecraft://?addExternalServer=StarrySky|mc.szzz666.top:19132");
     });
     //关闭窗口
     $(".popup  svg, .masking").click(function () {
@@ -185,3 +185,4 @@ $(document).ready(function () {
     });
 
 });
+
